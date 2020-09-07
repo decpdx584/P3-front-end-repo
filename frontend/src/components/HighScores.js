a7c09c8cbcd381f6e020212336bd00ab1b1f2701
blangwell <b.langwell@outlook.com>
2020-09-07 12:20:50 -0700

    [components] Add Favorites, HighScores, and Landing Components

---
+++ frontend/src/components/HighScores.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const HighScores = () => {
+  return(
+    <div>
+      <h1>Hi Scores</h1>
+      <h2>Most favorited by users</h2>
+    </div>
+  )
+}
+
+export default HighScores;
