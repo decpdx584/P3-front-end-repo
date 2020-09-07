433700e479be11ab04c1077902f89f6c814c3431
blangwell <b.langwell@outlook.com>
2020-09-07 15:32:57 -0700

    [style] Style page titles and subtitles

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Landing = (props) => {
   return(
     <div id="landing">
-      <h1>GArcade Landing</h1>
-      <h4>A Place for Project One</h4><br />
+      <h1 className="pixel-text">GArcade</h1>
+      <h4 className="sub-title">A Place for Project One</h4><br />
       <img id="cabinet-far" src="https://www.clipartkey.com/mpngs/m/52-522026_transparent-arcade-clipart-black-and-white-video-game.png"/>
+      <br />
       <a href="/login" >Login</a>
       <a href="/signup">Signup</a>
     </div>
