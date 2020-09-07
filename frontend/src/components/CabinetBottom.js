84fd3310da508ed0406648879678abf2863e89d2
blangwell <b.langwell@outlook.com>
2020-09-07 16:56:53 -0700

    [profile] Add info fields to /profile

--- frontend/src/components/CabinetBottom.js
+++ frontend/src/components/CabinetBottom.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const CabinetBottom = () => {
+const CabinetBottom = (props) => {
   return(
     <div>
       <h1>Cabinet Bottom</h1>
+      <p>This is where the buttons will live</p>
     </div>
   )
 }
--- frontend/src/components/CabinetScreen.js
+++ frontend/src/components/CabinetScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const CabinetScreen = () => {
+const CabinetScreen = (props) => {
   return(
     <div>
+      {/* here we will display the game and the game list */}
       <h1>Cabinet Screen</h1>
     </div>
   )
