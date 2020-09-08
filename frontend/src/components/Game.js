8cb47b3855faea68654ea18754479bbd81ef67f8
Martin Briceno <martin.briceno@live.com>
2020-09-07 20:25:37 -0400

    [game] autosize

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -6,12 +6,10 @@ const Game = () => {
         <div className="arcade">
         <h2>Revenge of the Flower</h2>
         <Iframe url={"https://mgustavob.github.io/Revenge-of-the-Flower/"}
-            width="60%"
-            height="800px"
             id="myId"
             className="myClassname"
             display="initial"
-            position="relative"
+            //position="relative"
             overflow="hidden" />
 
     </div>
