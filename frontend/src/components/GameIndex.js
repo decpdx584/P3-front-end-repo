0b3127ef0a46b1eb56c9953d34e54903cb8b7d2e
Martin Briceno <martin.briceno@live.com>
2020-09-09 15:29:41 -0400

    commits before pull

--- frontend/src/components/GameIndex.js
+++ frontend/src/components/GameIndex.js
@@ -15,7 +15,7 @@ const GameIndex = (props) => {
   //   })
   //   .catch(err => console.log('error getting server data \n', err))
   // }
-  
+
   let mapThemGames = () => {
     return gamesDisplayed.map((g, idx) => {
       console.log('THiS A G', g)
@@ -23,13 +23,13 @@ const GameIndex = (props) => {
         <div key={idx}>
         <h3 className="sub-title">{g.name ? g.name : g.title}</h3>
         <p>{g.gameUrl}</p>
-        <p>{g.description != 'none' ? g.description : 'no description uploaded'}</p>
+        <p>{g.description !== 'none' ? g.description : 'no description uploaded'}</p>
         </div>
       )
     })
-  } 
+  }
 
-  let decideGames = 
+  let decideGames =
     gamesDisplayed.length > 0 ? (
       <div>
         <h1 className="pixel-text">Them Games</h1>
@@ -38,7 +38,7 @@ const GameIndex = (props) => {
     ) : (
       <h1>Nothin in state rn 😓</h1>
     )
-  
+
   useEffect(() => {
     axios.get(`${REACT_APP_SERVER_URL}/api/games/arcade`)
     .then(response => {
@@ -51,10 +51,10 @@ const GameIndex = (props) => {
 
   // we need to wait for state to set and, once it does
   // we need to display the state's game information
-  // will this be promise based? probably? 
+  // will this be promise based? probably?
 
   return (
-    
+
     <div>
       {decideGames}
     </div>
